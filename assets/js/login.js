7480d92db7689d9ca9c6457e31931dd09b75eb2e
Twodogs-web <836977693@qq.com>
2021-09-09 17:42:30 +0800

    完成了主页功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,6 +1,6 @@
 $(function() {
     // 点击 去注册 的链接
-    $('#link_reg').on('click', function(){
+    $('#link_reg').on('click', function() {
         $('.login-box').hide()
         $('.reg-box').show()
     })
@@ -15,7 +15,7 @@ $(function() {
     // 从 layui 中获取 form 对象
     var form = layui.form
 
-    var layer = layui.msg
+    var layer = layui.layer
     // 通过 form。verify() 函数自定义校验规则
         form.verify({
             // 自定义了一个叫做 pwd 的密码校验规则
@@ -38,11 +38,12 @@ $(function() {
              // 1、阻止默认的提交行为
             e.preventDefault()
             //2、发起Ajax的post请求
-            var data = { username: $('#form_reg [name=username]').val(), password: 
-            $('#form_reg[name=password]').val() }
+            var data = { 
+                username: $('#form_reg [name=username]').val(), 
+                password: $('#form_reg [name=password]').val()
+             }
 
-            $.post('/api/reguser', 
-            data, function(res) {
+            $.post('/api/reguser', data, function(res) {
                 if(res.status !== 0) {
                     return layer.msg(res.message)
                 }
@@ -53,22 +54,23 @@ $(function() {
         })
 
         // 监听登录表单的提交事件
-        $('#form_login').submit(function (e) {
+        $('#form_login').submit(function(e) {
             //  阻止默认的提交行为
             e.preventDefault()
             $.ajax({
                 url: '/api/login',
                 method: 'POST',
+                // 快速获取表单中的数据
                 data: $(this).serialize(),
                 success: function(res) {
-                    if(res.status !== 0) {
+                    if (res.status !== 0) {
                         return layer.msg('登录失败！')
                     }
                     layer.msg('登录成功！ ')
                     // 将登录成功得到的token 字符串，存储到
-                    console.log(res.token)
+                    localStorage.setItem('token', res.token)
                     // 跳转到后台主页
-                    location.herf =  '/index.html'
+                    location.href =  '/index.html'
                 }
             })
         })
